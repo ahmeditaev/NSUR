75de1b39859e1db8053a486cf7b6a94b9578b256
Ahmed Itaev <ahmeditaev@gmail.com>
2018-03-20 11:15:04 +0600

    added detail page for news

--- NSUR/news/urls.py
+++ NSUR/news/urls.py
@@ -8,12 +8,12 @@ app_name = 'news'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.mainPage,name='mainPage'),
-    url(r'^about/$',views.AboutUs.as_view(),name='aboutUs'),
-    url(r'^rukovodstvo/$',views.Rukovodstvo.as_view(),name='rukovodstvo'),
-    url(r'^news/$',views.PostListView.as_view(queryset=News.objects.all().order_by('-published')[:20]),name='news'),
+    url(r'^$', views.mainPage, name='mainPage'),
+    url(r'^about/$', views.AboutUs.as_view(), name='aboutUs'),
+    url(r'^rukovodstvo/$', views.Rukovodstvo.as_view(), name='rukovodstvo'),
+    url(r'^news/$', views.PostListView.as_view(queryset=News.objects.all().order_by('-published')[:20]), name='news'),
     url(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='details'),
-    url(r'emailus/$',views.emailUs,name='emailUs')
+    url(r'emailus/$', views.emailUs, name='emailUs')
 
 
 ]
